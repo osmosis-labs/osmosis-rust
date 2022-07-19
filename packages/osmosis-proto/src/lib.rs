b2b00aa1ecce02c65b34d9a3a7842d87815e1fcc
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-07-19 15:47:17 +0700

    reduce module depth

--- packages/osmosis-proto/src/lib.rs
+++ packages/osmosis-proto/src/lib.rs
@@ -9,54 +9,52 @@ pub use cosmos_sdk_proto;
 pub const OSMOSISD_VERSION: &str = include_str!("prost/OSMOSIS_COMMIT");
 
 /// Osmosis protobuf definitions.
-pub mod osmosis {
-    pub mod epochs {
-        pub mod v1beta1 {
-            include!("prost/osmosis.epochs.v1beta1.rs");
-        }
+pub mod epochs {
+    pub mod v1beta1 {
+        include!("prost/osmosis.epochs.v1beta1.rs");
     }
+}
 
-    pub mod gamm {
-        pub mod v1beta1 {
-            include!("prost/osmosis.gamm.v1beta1.rs");
-        }
+pub mod gamm {
+    pub mod v1beta1 {
+        include!("prost/osmosis.gamm.v1beta1.rs");
     }
-    pub mod incentives {
-        include!("prost/osmosis.incentives.rs");
-    }
+}
+pub mod incentives {
+    include!("prost/osmosis.incentives.rs");
+}
 
-    pub mod lockup {
-        include!("prost/osmosis.lockup.rs");
-    }
+pub mod lockup {
+    include!("prost/osmosis.lockup.rs");
+}
 
-    pub mod mint {
-        pub mod v1beta1 {
-            include!("prost/osmosis.mint.v1beta1.rs");
-        }
+pub mod mint {
+    pub mod v1beta1 {
+        include!("prost/osmosis.mint.v1beta1.rs");
     }
+}
 
-    pub mod poolincentives {
-        pub mod v1beta1 {
-            include!("prost/osmosis.poolincentives.v1beta1.rs");
-        }
+pub mod poolincentives {
+    pub mod v1beta1 {
+        include!("prost/osmosis.poolincentives.v1beta1.rs");
     }
+}
 
-    pub mod superfluid {
-        include!("prost/osmosis.superfluid.rs");
-        pub mod v1beta1 {
-            include!("prost/osmosis.superfluid.v1beta1.rs");
-        }
+pub mod superfluid {
+    include!("prost/osmosis.superfluid.rs");
+    pub mod v1beta1 {
+        include!("prost/osmosis.superfluid.v1beta1.rs");
     }
+}
 
-    pub mod tokenfactory {
-        pub mod v1beta1 {
-            include!("prost/osmosis.tokenfactory.v1beta1.rs");
-        }
+pub mod tokenfactory {
+    pub mod v1beta1 {
+        include!("prost/osmosis.tokenfactory.v1beta1.rs");
     }
+}
 
-    pub mod txfees {
-        pub mod v1beta1 {
-            include!("prost/osmosis.txfees.v1beta1.rs");
-        }
+pub mod txfees {
+    pub mod v1beta1 {
+        include!("prost/osmosis.txfees.v1beta1.rs");
     }
 }
