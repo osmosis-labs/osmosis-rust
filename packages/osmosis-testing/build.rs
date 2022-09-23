5531fe52bf3e6d8df50013c10785cc0eddc4aa12
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-09-23 14:41:51 +0700

    write bindings to out dir instead of src

--- packages/osmosis-testing/build.rs
+++ packages/osmosis-testing/build.rs
@@ -54,17 +54,13 @@ fn main() {
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .raw_line("#![allow(non_upper_case_globals)]")
-        .raw_line("#![allow(non_camel_case_types)]")
-        .raw_line("#![allow(non_snake_case)]")
-        .raw_line("#![allow(dead_code)]")
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = manifest_dir.join("src");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
