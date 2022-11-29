b83db90e1198208fbe11c6a0ca0f39b2cb9f835a
George McAskill <42020095+georgemc98@users.noreply.github.com>
2022-11-28 19:44:29 -0600

    fix: update osmosis rev

--- packages/proto-build/src/lib.rs
+++ packages/proto-build/src/lib.rs
@@ -18,7 +18,7 @@ mod transformers;
 const COSMOS_SDK_REV: &str = "v0.45.0-osmo-v12";
 
 /// The osmosis commit or tag to be cloned and used to build the proto files
-const OSMOSIS_REV: &str = "v12.0.0";
+const OSMOSIS_REV: &str = "main";
 
 // All paths must end with a / and either be absolute or include a ./ to reference the current
 // working directory.
