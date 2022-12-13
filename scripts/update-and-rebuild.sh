8f8b8aa4490ab371caaa4fef09dd165241d49ede
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-12-13 16:58:50 +0700

    [workdlow][debug] diff

--- scripts/update-and-rebuild.sh
+++ scripts/update-and-rebuild.sh
@@ -9,18 +9,20 @@ OSMOSIS_REV=${1:-main}
 PROTO_BUILD_MAIN_RS="$SCRIPT_DIR/../packages/proto-build/src/main.rs"
 sed "s/const OSMOSIS_REV: \&str = \".*\";/const OSMOSIS_REV: \&str = \"$OSMOSIS_REV\";/g" "$PROTO_BUILD_MAIN_RS" | tee -i "$PROTO_BUILD_MAIN_RS"
 
-# rebuild osmosis-std
-cd "$SCRIPT_DIR/../packages/proto-build/" && cargo run -- --update-deps
+git diff
 
-# if dirty or untracked file exists
-if [[ $(git diff --stat) != '' ||  $(git ls-files  --exclude-standard  --others) ]]; then
-  # add, commit and push
-  git add "$SCRIPT_DIR/.."
-  git commit -m "rebuild with $(git rev-parse --short HEAD:dependencies/osmosis)"
+# # rebuild osmosis-std
+# cd "$SCRIPT_DIR/../packages/proto-build/" && cargo run -- --update-deps
 
-  BRANCH="autobuild-$OSMOSIS_REV"
-  git checkout -b "$BRANCH"
-  git push -uf origin "$BRANCH"
-else
-  echo '[CLEAN] No update needed for this build'
-fi
+# # if dirty or untracked file exists
+# if [[ $(git diff --stat) != '' ||  $(git ls-files  --exclude-standard  --others) ]]; then
+#   # add, commit and push
+#   git add "$SCRIPT_DIR/.."
+#   git commit -m "rebuild with $(git rev-parse --short HEAD:dependencies/osmosis)"
+
+#   BRANCH="autobuild-$OSMOSIS_REV"
+#   git checkout -b "$BRANCH"
+#   git push -uf origin "$BRANCH"
+# else
+#   echo '[CLEAN] No update needed for this build'
+# fi
