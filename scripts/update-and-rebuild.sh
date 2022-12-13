596aa7ad11457f8a9837b4148fcd5492835c6c98
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-12-13 16:45:01 +0700

    [workdlow][debug] remove cat

--- scripts/update-and-rebuild.sh
+++ scripts/update-and-rebuild.sh
@@ -9,10 +9,8 @@ OSMOSIS_REV=${1:-main}
 PROTO_BUILD_MAIN_RS="$SCRIPT_DIR/../packages/proto-build/src/main.rs"
 sed "s/const OSMOSIS_REV: \&str = \".*\";/const OSMOSIS_REV: \&str = \"$OSMOSIS_REV\";/g" "$PROTO_BUILD_MAIN_RS" | tee -i "$PROTO_BUILD_MAIN_RS"
 
-cat "$PROTO_BUILD_MAIN_RS"
-
 # rebuild osmosis-std
-# cd "$SCRIPT_DIR/../packages/proto-build/" && cargo run -- --update-deps
+cd "$SCRIPT_DIR/../packages/proto-build/" && cargo run -- --update-deps
 
 # if dirty or untracked file exists
 if [[ $(git diff --stat) != '' ||  $(git ls-files  --exclude-standard  --others) ]]; then
