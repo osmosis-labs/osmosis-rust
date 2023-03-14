355861fbb5bb5718a486de0f264b92f4941dea7d
larry <26318510+larry0x@users.noreply.github.com>
2023-03-14 15:50:59 +0000

    remove private imports

--- packages/proto-build/src/transform.rs
+++ packages/proto-build/src/transform.rs
@@ -1,12 +1,12 @@
 use heck::ToUpperCamelCase;
 use log::debug;
 use prost_types::FileDescriptorSet;
+use quote::ToTokens;
 use regex::Regex;
 use std::ffi::OsStr;
 use std::fs::{create_dir_all, remove_dir_all};
 use std::path::{Path, PathBuf};
 use std::{fs, io};
-use syn::__private::ToTokens;
 use syn::{parse_quote, File, Item, ItemMod};
 use walkdir::WalkDir;
 
