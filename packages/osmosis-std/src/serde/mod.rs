165a60e69d8b45eaf5eb9530e9b4d87d2382c37a
sampocs <sam.pochyly@gmail.com>
2023-08-28 13:59:15 -0500

    updated stableswap pool to handle deserializing strings in the scaling factors

--- packages/osmosis-std/src/serde/mod.rs
+++ packages/osmosis-std/src/serde/mod.rs
@@ -20,3 +20,30 @@ pub mod as_str {
         serializer.serialize_str(&value.to_string())
     }
 }
+
+pub mod as_str_vec {
+    use serde::{de, Deserialize, Deserializer, Serializer, Serialize};
+    use std::{fmt::Display, str::FromStr};
+
+    pub fn deserialize<'de, T, D>(deserializer: D) -> Result<Vec<T>, D::Error>
+    where
+        T: FromStr,
+        T::Err: Display,
+        D: Deserializer<'de>,
+    {
+        let vec_of_strings: Vec<String> = Vec::deserialize(deserializer)?;
+        vec_of_strings
+            .into_iter()
+            .map(|s| T::from_str(&s).map_err(de::Error::custom))
+            .collect()
+    }
+
+    pub fn serialize<S, T>(values: &Vec<T>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+        T: Display,
+    {
+        let vec_of_strings: Vec<String> = values.iter().map(|value| value.to_string()).collect();
+        vec_of_strings.serialize(serializer)
+    }
+}
